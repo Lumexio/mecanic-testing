f3ba8ccd1cd2b63e045c09f8902f12a9141151e2
Piaczio <trotroyu@gmail.com>
2021-05-13 19:27:40 -0600

    css percentage

--- js/mecanic.js
+++ js/mecanic.js
@@ -1,29 +1,12 @@
-
 var textoriginal = 'Codigo shift+ctrl+a'
 document.getElementById('original').innerHTML = textoriginal;
-
-
-
 function copiadora() {
- var copia = textoriginal;//Guarda array en la otra variable
-
- const textArea = document.createElement('textarea');//copia en un pre el contenido de las etiquetas
-
- document.getElementById('copia').innerHTML = copia;//muestra lo piado en la segunda etiqueta
-
- textArea.textContent = copia;//guarda el array en el contenido
- document.body.append(textArea);//crea un elemento
- textArea.select();//selecciona el elemento
- document.execCommand("copy");//copia el texto dentro del elemento
+ var copia = textoriginal;/*Guarda array en la otra variable*/
+ const textArea = document.createElement('textarea');/*copia en un pre el contenido de las etiquetas*/
+ document.getElementById('copia').innerHTML = copia;/*muestra lo piado en la segunda etiqueta*/
+ textArea.textContent = copia;/*guarda el array en el contenido*/
+ document.body.append(textArea);/*crea un elemento*/
+ textArea.select();/*selecciona el elemento*/
+ document.execCommand("copy");/*copia el texto dentro del elemento*/
  textArea.remove();//elimina elemento
 }
-
-
-
-
-
-
-
-
-
-
