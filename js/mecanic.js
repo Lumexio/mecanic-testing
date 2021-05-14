f5f6340f33eba81c121508ba3a4c137380f8edb1
Piaczio <trotroyu@gmail.com>
2021-05-13 19:02:59 -0600

    copiar en  clipboard

--- js/mecanic.js
+++ js/mecanic.js
@@ -6,15 +6,16 @@ document.getElementById('original').innerHTML = textoriginal;
 
 function copiadora() {
  var copia = textoriginal;//Guarda array en la otra variable
- const copyText = document.getElementById("precopia").textContent;
+ //const copyText = document.getElementById("precopia").textContent;
  const textArea = document.createElement('textarea');
 
  document.getElementById('copia').innerHTML = copia;
 
  textArea.textContent = copia;
-
+ document.body.append(textArea);
  textArea.select();
  document.execCommand("copy");
+ textArea.remove();
 
 
 }
