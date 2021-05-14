5062c01be0667bdc3064f50b09fcba6fcec8fb04
Piaczio <trotroyu@gmail.com>
2021-05-13 19:06:07 -0600

    dicumenting

--- js/mecanic.js
+++ js/mecanic.js
@@ -6,18 +6,16 @@ document.getElementById('original').innerHTML = textoriginal;
 
 function copiadora() {
  var copia = textoriginal;//Guarda array en la otra variable
- //const copyText = document.getElementById("precopia").textContent;
- const textArea = document.createElement('textarea');
 
- document.getElementById('copia').innerHTML = copia;
+ const textArea = document.createElement('textarea');//copia en un pre el contenido de las etiquetas
 
- textArea.textContent = copia;
- document.body.append(textArea);
- textArea.select();
- document.execCommand("copy");
- textArea.remove();
+ document.getElementById('copia').innerHTML = copia;//muestra lo piado en la segunda etiqueta
 
-
+ textArea.textContent = copia;//guarda el array en el contenido
+ document.body.append(textArea);//crea un elemento
+ textArea.select();//selecciona el elemento
+ document.execCommand("copy");//copia el texto dentro del elemento
+ textArea.remove();//elimina elemento
 }
 
 
