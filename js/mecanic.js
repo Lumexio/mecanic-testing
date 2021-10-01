e873f57e14609dab8a58fd8e4accb5589f9c8532
Piaczio <trotroyu@gmail.com>
2021-10-01 13:52:52 -0600

    fix: new face flex

--- js/mecanic.js
+++ js/mecanic.js
@@ -1,10 +1,11 @@
-var textoriginal = 'Codigo shift+ctrl+a'
-document.getElementById('original').innerHTML = textoriginal;
+let input = "";
+let output
 function copiadora() {
- var copia = textoriginal;/*Guarda array en la otra variable*/
+ input = document.getElementById('input').value;
+ output = input;/*Guarda array en la otra variable*/
  const textArea = document.createElement('textarea');/*copia en un pre el contenido de las etiquetas*/
- document.getElementById('copia').innerHTML = copia;/*muestra lo piado en la segunda etiqueta*/
- textArea.textContent = copia;/*guarda el array en el contenido*/
+ document.getElementById('output').innerHTML = output;/*muestra lo copiado en la segunda etiqueta*/
+ textArea.textContent = output;/*guarda el array en el contenido*/
  document.body.append(textArea);/*crea un elemento*/
  textArea.select();/*selecciona el elemento*/
  document.execCommand("copy");/*copia el texto dentro del elemento*/
